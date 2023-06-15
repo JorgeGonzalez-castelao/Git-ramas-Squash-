27b1f11df3eb438155ba3e4ba25849ce55bee759
Jorge Gonzalez Ruiz <jgonzalezruiz@danielcastelao.org>
2023-06-15 10:25:49 +0200

    # Conflicts: #	src/Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(" Me gusta el chocolate");
+        System.out.println("pero con almendras");
+        System.out.println("tambien la vainilla");
+        System.out.println("el saborizante proviene de las glandulas de los castores");
         System.out.println("Me gusta el chocolate blanco");
         System.out.println("Me encanta el helado de fresa");
         System.out.println("Pero mejor que nada el cafe");
