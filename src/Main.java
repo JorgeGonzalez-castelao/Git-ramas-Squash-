4635b53a80431d2e8b1ce96ce44e5ca3723fba0a
Jorge Gonzalez Ruiz <jgonzalezruiz@danielcastelao.org>
2023-06-15 10:16:30 +0200

    primer commit colaborador

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println(" Me gusta el chocolate");
+        System.out.println("Me gusta el chocolate blanco");
     }
 
 }
