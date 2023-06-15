a03b7ca0959f153d30266bac569aa19789b7de04
Jorge Gonzalez Ruiz <jgonzalezruiz@danielcastelao.org>
2023-06-15 10:10:36 +0200

    segundo commit lider

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println(" Me gusta el chocolate");
         System.out.println("pero con almendras");
+        System.out.println("tambien la vainilla");
     }
 
 }
